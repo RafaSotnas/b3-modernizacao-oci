1ae1e92a2c4a014bf868c295fe8a613a12496e9d
rafael oliveira <rafasotnas@gmail.com>
2026-05-12 10:32:46 -0300

    pronto

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = ">= 5.0"
-    }
-  }
-}
-
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
