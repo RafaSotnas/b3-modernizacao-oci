45deea7293be0daf8d1f0956367a5e59272649dc
rafael oliveira <rafasotnas@gmail.com>
2026-05-12 10:27:56 -0300

    delete

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 5.0"
+    }
+  }
+}
+
 # Exemplo de configuração de Logs via Service Connector Hub
 # Aqui o conector envia logs de um log group OCI para um tópico de Notifications
 
--- modules/oke/main.tf
+++ modules/oke/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 5.0"
+    }
+  }
+}
+
 resource "oci_containerengine_cluster" "b3_cluster" {
   compartment_id = var.compartment_id
 
